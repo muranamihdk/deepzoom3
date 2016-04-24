988ea466e402f78daad7a76e07bb442db487df99
muranamihdk <muranamihdk@users.noreply.github.com>
2016-04-24 20:19:52 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+setup (
+    name='DeepZoomTools',
+    version='0.1.0',
+    description='Python tool for generating a DZI file and tiled image files for Deep Zoom',
+    url='https://github.com/muranamihdk/deepzoom3',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='deepzoom seadragon dzi dzc seadragonajax seadragonmobile silverlightdeepzoom microsoft openzoom',
+    py_modules=['deepzoom'],
+    install_requires=['pillow'],
+)
